036cc968e73b5cfd7623ff083d872da5eb037f50
tymursafin <113561665+tymursafin@users.noreply.github.com>
2022-12-20 06:04:23 +0300

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,5 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first class");
+        
+        System.out.println("changes in first class");
     }
 }
